23f45c59a02b646d0ccdcff8871d394bc5fd7555
vmnchv <vmnchv@gmail.com>
2018-12-16 17:47:57 +0300

    Задание 9

---
+++ Lesson1/Lesson_1_9.py
@@ -0,0 +1,11 @@
+print('Введите три числа: ')
+a = int(input())
+b = int(input())
+c = int(input())
+
+if b < a < c or c < a < b:
+    print('Среднее:', a)
+elif a < b < c or c < b < a:
+    print('Среднее:', b)
+else:
+    print('Среднее:', c)
